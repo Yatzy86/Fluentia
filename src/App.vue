69bd6e524ec3055e2c5b30cf76e8704b57400abe
parama23 <paramakarmaker23@gmail.com>
2026-02-24 11:10:01 +0100

    new

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
 import AppHeader from "./components/AppHeader.vue";
 import "./assets/style.scss";
 </script>
-=======
-<script setup></script>
->>>>>>> d2fe403a2c914e6d3359dda5fd5a7f33510a4625
 
 <template>
   <div>
