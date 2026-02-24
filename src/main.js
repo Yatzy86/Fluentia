6fca5f3941f83037c545e96b748b219ec5777a1f
Wimlan <wilma.yasuda@gmail.com>
2026-02-24 11:01:29 +0100

    edited main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createBootstrap } from "bootstrap-vue-next";
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
 import "./assets/style.scss";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(createBootstrap()).mount("#app");
