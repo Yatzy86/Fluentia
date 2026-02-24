bb513aaae57f33f7ba91977e0e2332cfe40ada3f
Wimlan <wilma.yasuda@gmail.com>
2026-02-24 11:20:20 +0100

    updated imports for memory game

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@ import AppHeader from "./components/AppHeader.vue";
 <template>
   <div>
     <AppHeader />
-    <RouterView />
     <h1>Vårat projekt</h1>
     <h2>Här är vi</h2>
     <RouterLink :to="{ name: 'game', params: { name: 'memory' } }"
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -3,10 +3,14 @@ import { computed, defineAsyncComponent } from "vue";
 const props = defineProps(["name"]);
 //gör egen variabel för att ladda in spel
 //lägg till alla spel
+const memoryGame = defineAsyncComponent(
+  () => import("../components/MemoryGame.vue"),
+);
 
+//Gör till en metod?
 const currentGame = computed(() => {
   if (props.name === "memory") {
-    return defineAsyncComponent(() => import("../components/Memory.vue"));
+    return memoryGame;
   }
 });
 </script>
