b9dbb439aba2f1b0540607b3edca4040c597ec47
Wimlan <wilma.yasuda@gmail.com>
2026-02-26 15:32:35 +0100

    added and slightly styled bootstrap card for intro section

---
+++ src/components/PageIntro.vue
@@ -0,0 +1,15 @@
+<script></script>
+<template>
+  <section id="intro_section">
+    <b-card title="Learn Swedish in a fun way">
+      <b-card-text> Play Games, build vocabulary, track progress </b-card-text>
+
+      <RouterLink to="/dictionary" class="card-link"
+        ><b-button>Open Dictionary</b-button></RouterLink
+      >
+      <RouterLink to="#" class="card-link"
+        ><b-button>Open Glossary</b-button></RouterLink
+      >
+    </b-card>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
 import GameLinks from "../components/GameLinks.vue";
+import PageIntro from "../components/PageIntro.vue";
 </script>
 
 <template>
+  <PageIntro />
   <!-- spel lista sektion -->
   <GameLinks />
 </template>
