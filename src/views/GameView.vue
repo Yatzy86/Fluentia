4d4dc19c6993f52eb3c96df8af17add665a1b87d
Wimlan <wilma.yasuda@gmail.com>
2026-03-02 16:05:38 +0100

    la till länk längst upp och sektion för glossary

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { computed, defineAsyncComponent } from "vue";
+import { computed, defineAsyncComponent, ref } from "vue";
+import { RouterLink } from "vue-router";
 
 //sparar routerlink propsen i ett props variabel objekt
 //"name" blir namnet för propsen som man skickade in med routerlink
@@ -34,9 +35,53 @@ const currentGame = computed(() => {
     return "Couldn't find game.";
   }
 });
+
+//länkar variabel till v-model
+let searchedWord = defineModel();
+let addedWord = ref("");
+
+//När knappen trycks på så blir addedWord samma som searchedWord
+// addedWord skrivs sedan ut längre ner på sidan
+function update() {
+  addedWord.value = searchedWord.value;
+}
 </script>
 
 <template>
-  <!-- laddar komponenten beroende på vad variabeln currentGame blir -->
-  <component :is="currentGame" />
+  <article class="d-flex flex-column align-items-center">
+    <!-- länk till homeview -->
+    <section>
+      <h1 id="title_nav">
+        <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+        / {{ props.name }}
+      </h1>
+    </section>
+
+    <!-- laddar komponenten beroende på vad variabeln currentGame blir -->
+    <component :is="currentGame" />
+
+    <!-- add to glossary sektion -->
+    <section id="add_glossary">
+      <b-card variant="secondary" title="Add to Glossary">
+        <b-card-text v-if="addedWord.length > 0"
+          >You have added the word: {{ addedWord }}!</b-card-text
+        >
+        <b-card-text>
+          <b-input-group class="mt-3">
+            <b-form-input
+              variant="fourth"
+              v-model="searchedWord"
+            ></b-form-input>
+            <b-input-group-append>
+              <b-button variant="third" @click="update">Button</b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-card-text>
+
+        <RouterLink to="/glossary" class="card-link">
+          Explore your Glossary>
+        </RouterLink>
+      </b-card>
+    </section>
+  </article>
 </template>
