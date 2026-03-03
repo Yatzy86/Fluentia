4ec684d2df5e4838c446d5e9f35c80d03f644930
parama23 <paramakarmaker23@gmail.com>
2026-03-03 11:42:42 +0100

    Instructions for Memory game

--- src/components/games/MemoryGame.vue
+++ src/components/games/MemoryGame.vue
@@ -1,1 +1,23 @@
-<template><h1>Memory</h1></template>
+<script setup>
+import { ref } from "vue";
+
+const showInstructions = ref(true);
+
+function closeInstructions() {
+  showInstructions.value = false;
+}
+</script>
+
+<template>
+  <BModal
+    v-model="showInstructions"
+    ok-title="Got it"
+    title="Instructions!"
+    @click="closeInstructions"
+  >
+    <p>1. Click two cards to flip them.</p>
+    <p>2. If they match they stay open.</p>
+    <p>3. If not they flip back.</p>
+    <p>4. Match all pairs to win.</p>
+  </BModal>
+</template>
