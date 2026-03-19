1855852a7b7072e26647541e0de1bc4ba28bb014
hhofund <mv.dano2004@gmail.com>
2026-03-19 10:49:16 +0100

    kopplat levelsystem till choicequiz

--- src/components/games/ChoicequizGame.vue
+++ src/components/games/ChoicequizGame.vue
@@ -2,7 +2,8 @@
 import { ref } from "vue";
 
 import { useRouter } from "vue-router";
-
+import { useLevelStore } from "../LevelSystem.js"
+const levelStore = useLevelStore()
 const showInstructions = ref(true);
 const router = useRouter();
 
@@ -157,6 +158,7 @@ const checkAnswer = (chosenAnswer) => {
     chosenQuestions.value[currentQuestion.value].rightAnswer === chosenAnswer
   ) {
     score.value++;
+    levelStore.addXP(100)
   }
   answered.value = true;
 
