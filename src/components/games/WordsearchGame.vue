c702b888466a89c1a5885f5660b400c0b1cea4d8
Alexander Andersson <karlalexanderandersson@gmail.com>
2026-03-20 21:10:11 +0100

    Lagt till lite kommentarer och stylat knappen

--- src/components/games/WordsearchGame.vue
+++ src/components/games/WordsearchGame.vue
@@ -3,6 +3,7 @@ import WordRow from "../WordRow.vue";
 import SimpleKeyboard from "../SimpleKeyboard.vue";
 import { reactive, onMounted, computed, ref } from "vue";
 
+//Här finns både de svenska och engelska översättningarna
 const word = [
   {
     swedish: "äpple",
@@ -57,27 +58,45 @@ const word = [
     english: "store",
   },
 ];
-
+//Startar om tangentbordet
 const restartKeyboard = ref(false);
+//Startar om orden
 const restartWords = ref(false);
+//Denna blandar ord med hjälp av math.floor och math.random.
 const randomWord = ref(word[Math.floor(Math.random() * word.length)]);
+//Denna blandar de svenska orden
 const randomWordSwe = ref(randomWord.value.swedish);
+//Denna blandar de engelska orden
 const randomWordEng = ref(randomWord.value.english);
+//Denna gömmer ledtråden
 const hintOpen = ref(false);
 
+//Här är funktionen för newGame
+//Den blandar orden ifrån word variabeln
+//Därefter blandas de i både svenska och engleska och lägger sig i randomWordSwe.value och randomWordEng.value
+//När det blandats så lägger den sig i state.solution
+//Startar med 6 gissningar(state.guesses)
+//Startar på 0 gissningar(state.currentGuessIndex)
+//Startar om tangentborden och orden
 const newGame = () => {
   randomWord.value = word[Math.floor(Math.random() * word.length)];
   randomWordSwe.value = randomWord.value.swedish;
   randomWordEng.value = randomWord.value.english;
-
   state.solution = randomWordSwe;
   state.guesses = ["", "", "", "", "", ""];
   state.currentGuessIndex = 0;
-
   restartKeyboard.value = true;
   restartWords.value = true;
 };
 
+//I state ligger solution där det svenska ordet blandas och läggs in
+//6 gissningar
+//Nuvarande frågor på 0
+//Här är guessed letters miss, found och hint
+//De är kopplade till tangentbordet och klick
+//Hint betydet att bokstaven finns
+//Miss att den inte finns
+//Found ifall den är på helt rätt plats
 const state = reactive({
   solution: randomWordSwe,
   guesses: ["", "", "", "", "", ""],
@@ -88,17 +107,20 @@ const state = reactive({
     hint: [],
   },
 });
-
+//Denna funktionen startar false men blir här true eftersom denna är kopplad till
+//knappen showHide
 const toggleHint = () => {
   hintOpen.value = !hintOpen.value;
 };
-
+//Ifall du vinner
 const wonGame = computed(
   () => state.guesses[state.currentGuessIndex - 1] === state.solution,
 );
-
+//Ifall du förlorar
 const lostGame = computed(() => !wonGame.value && state.currentGuessIndex >= 6);
 
+//Funktionen handleInput tar med key som parameter
+//Om är like med eller mindre än 6 så vinner du
 const handleInput = (key) => {
   if (state.currentGuessIndex >= 6 || wonGame.value) {
     return;
@@ -168,11 +190,11 @@ onMounted(() => {
     style="max-width: 28rem"
   >
     <div @click="toggleHint" v-if="!hintOpen">
-      <button>Show in english</button>
+      <button class="btn btn-third showHide">Show in english</button>
     </div>
     <div v-else>
-      <button @click="toggleHint">Hide</button>
-      <p>{{ randomWordEng }}</p>
+      <button @click="toggleHint" class="btn btn-third showHide">Hide</button>
+      <p class="score">{{ randomWordEng }}</p>
     </div>
     <div>
       <word-row
@@ -212,5 +234,17 @@ onMounted(() => {
   color: #fff;
   font-size: 1.25rem;
   text-align: center;
+}
+.score {
+  color: #fff;
+  font-size: 1.25rem;
+  text-align: center;
+}
+.showHide {
+  display: flex;
+  padding: 0.5rem;
+  background-color: #e7c558;
+  border-radius: 0.5rem;
+  margin: auto;
 }
 </style>
