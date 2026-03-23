efbdd4817a3348d73828a96da74b6050e045eb40
parama23 <paramakarmaker23@gmail.com>
2026-03-23 22:08:56 +0100

    Added comments

--- src/components/ShareButton.vue
+++ src/components/ShareButton.vue
@@ -1,8 +1,12 @@
 <template>
+  <!--Knapp som kör funktionen "share" när man klickar-->
   <button @click="share" class="share-btn">
+    <!--Ikon för dela-->
     <img src="../assets/img/share.png" alt="Share" class="share-icon" />
   </button>
+  <!--Container som placerar toasten nere till höger-->
   <div class="toast-container position-fixed bottom-0 end-0 p-3">
+    <!--Toast visas bara om showToast är true-->
     <div
       v-if="showToast"
       class="toast show align-items-center text-white bg-primary border-0"
@@ -10,8 +14,11 @@
       aria-live="assertive"
       aria-atomic="true"
     >
+      <!--Flexbox för att lägga text och knapp bredvid varandra-->
       <div class="d-flex">
+        <!--Meddelandet-->
         <div class="toast-body">Link copied!</div>
+        <!--Stäng-knapp (X) som gömmer toasten direkt-->
         <button
           type="button"
           class="btn-close btn-close-white me-2 m-auto"
@@ -24,19 +31,23 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref } from "vue"; //ref för att skapa reaktiv variabel
 
-const showToast = ref(false);
+const showToast = ref(false); //Variabel som styr om toasten ska visas eller inte
 
+//Funktion körs när man klickar på knappen
 async function share() {
   const url = window.location.href; //Hämtar nuvarande sidans URL
   try {
     await navigator.clipboard.writeText(url); //Kopierar URL till Clipboard
-    showToast.value = true;
+    showToast.value = true; //Visar toasten
+
+    //Gömmer toasten efter 2 sekunder
     setTimeout(() => {
       showToast.value = false;
     }, 2000);
   } catch (error) {
+    // loggar fel om kopiering misslyckas
     console.error("Could not copy Link:", error);
   }
 }
